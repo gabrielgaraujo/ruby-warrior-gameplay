933ee75ab25922234dc6ae9b94097808380f7692
Gabriel Araujo <gabrielg.araujo@hotmail.com>
2018-05-10 00:08:09 +0200

    Wrap warrior into a Warrior class

--- butcheco-intermediate/player.rb
+++ butcheco-intermediate/player.rb
@@ -1,5 +1,27 @@
 class Player
   def play_turn(warrior)
-    # add your code here
+    warrior = Warrior.new(warrior)
+  end
+
+  class Warrior
+    def initialize(warrior)
+      @warrior = warrior
+    end
+
+    def direction_of_stairs
+      warrior.direction_of_stairs
+    end
+
+    def feel
+      warrior.feel
+    end
+
+    def walk!
+      warrior.walk!
+    end
+
+    private
+
+    attr_reader :warrior
   end
 end
