e7d8e49ab27b39d31f8998011d7aafbc9793e8f7
Gabriel Araujo <gabrielg.araujo@hotmail.com>
2018-05-10 00:50:19 +0200

    Add new discovered abilitites

--- butcheco-intermediate/player.rb
+++ butcheco-intermediate/player.rb
@@ -9,18 +9,37 @@ class Player
       @warrior = warrior
     end
 
+    # Attacks a unit in given direction (:forward by default).
+    def attack!(direction: :forward)
+      warrior.attack!
+    end
+
+    # Returns the direction (:left, :right, :forward, :backward) the stairs are from your location.
     def direction_of_stairs
       warrior.direction_of_stairs
     end
 
-    def feel
+    # Returns a Space for the given direction (:forward by default).
+    def feel(direction: :forward)
       warrior.feel
     end
 
+    # Returns an integer representing your health.
+    def health
+      warrior.health
+    end
+
+    # Gain 10% of max health back, but do nothing more.
+    def rest!
+      warrior.rest!
+    end
+
+    # Move in the given direction (:forward by default).
     def walk!(direction: :forward)
       warrior.walk!(direction)
     end
 
+    # Walk towards the stairs.
     def walk_to_stairs!
       walk!(direction: direction_of_stairs)
     end
