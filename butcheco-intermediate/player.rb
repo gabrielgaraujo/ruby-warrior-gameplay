eccaef8fd449d6197ad9f2bd7c3ed013c5faecb8
Gabriel Araujo <gabrielg.araujo@hotmail.com>
2018-05-10 01:55:58 +0200

    Add new discovered abilities

--- butcheco-intermediate/player.rb
+++ butcheco-intermediate/player.rb
@@ -9,6 +9,15 @@ class Player
       @warrior = warrior
     end
 
+    # Walk towards the stairs.
+    def walk_to_stairs!
+      walk!(direction_of_stairs)
+    end
+
+    private
+
+    attr_reader :warrior
+
     # Attacks a unit in given direction (:forward by default).
     def attack!(direction = :forward)
       warrior.attack!(direction)
@@ -18,19 +27,29 @@ class Player
       feel(direction).enemy? ? attack!(direction) : warrior.walk!(direction)
     end
 
+    # Binds a unit in given direction to keep him from moving (forward by default).
+    def bind!(direction = :forward)
+      warrior.bind!(direction)
+    end
+
     # Returns the direction (:left, :right, :forward, :backward) the stairs are from your location.
     def direction_of_stairs
       warrior.direction_of_stairs
     end
 
     # Returns a Space for the given direction (:forward by default).
-  def feel(direction = :forward)
+    def feel(direction = :forward)
       warrior.feel(direction)
     end
 
     # Returns an integer representing your health.
     def health
       warrior.health
+    end
+
+    # Rescue a captive from his chains (earning 20 points) in given direction (forward by default).
+    def rescue!(direction = :forward)
+      warrior.rescue!(direction)
     end
 
     # Gain 10% of max health back, but do nothing more.
@@ -42,14 +61,5 @@ class Player
     def walk!(direction = :forward)
       attack_or_walk!(direction)
     end
-
-    # Walk towards the stairs.
-    def walk_to_stairs!
-      walk!(direction_of_stairs)
-    end
-
-    private
-
-    attr_reader :warrior
   end
 end
